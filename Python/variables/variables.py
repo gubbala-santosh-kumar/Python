7eae910c2eb524fa7aea5c9cc2b1455080648bff
GUBBALA SANTOSH KUMAR <gubbalasantoshkumar2004@gmail.com>
2024-07-19 19:14:40 +0530

    Add files via upload

---
+++ Python/variables/variables.py
@@ -0,0 +1,28 @@
+# Python Varaibles Concept:
+
+"""
+    In this Python we have some rules for creating variables:
+    --Rules--
+     -> varaibles must starts with (a-z),(A-Z) or ( _ )
+     -> should not starts whith a Number or any other special Characters
+     -> Variable name are case sensitive for example:  name != NAME
+     -> For assign a value to Variables we must use an Operator called as Assignment Operator (=)
+"""
+
+#  See some examples for variables :
+
+Python_Father_Name = "Guido van Rossum"    #---> String Variable
+
+Python_Introduced_Year = 1991              #---> Integer Variable 
+
+Current_Python_Version = 3.7               #---> Float Variable
+
+Does_python_is_easy_to_learn = True      #---> Boolean Variable
+
+print("Father of Python :",Python_Father_Name)
+
+print("Introduced in :",Python_Introduced_Year)
+
+print("Current Python Version", Current_Python_Version)
+
+print("Does Python is easy to learn : ", Does_python_is_easy_to_learn)
